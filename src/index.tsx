839c16ea849b34b75fea30d19beee1b66292ea1d
Carlos A. <carlosvsk@gmail.com>
2025-05-07 17:20:16 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,216 +1,388 @@
-// ---------- import React Packs
-import React from "react";
-import * as RN from "react-native";
 
-// ---------- import Variables Pack
-import { create } from "zustand";
 
-// ---------- import Local Tools
-import * as Elements from "./tools/base/Elements";
-import { Project } from "./tools/base/project";
+          // ---------- import React Packs
+          import React from 'react';
+          import * as RN from 'react-native';
 
-// ---------- set Caps Inputs
-const currRoute = "home";
+          // ---------- import Variables Pack
+          import { create } from 'zustand';
 
-let args: any = [];
+          // ---------- import Local Tools
+          import { mapElements } from './tools/base/project/mapElements';
+          import * as functions from './tools/base/functions';
+          import * as Elements from './tools/base/Elements';
+          import { Project } from './tools/base/project';
+          import * as jsvals from './tools/base/jsvals';
+          import { props } from './tools/base/props';
+          import * as customs from './tools/customs';
+          import * as stls from './tools/base/stls';
+          import { tools } from './tools';
 
-const screens = [
-  (...args: any) => (
-    <Elements.Screen3
-      pass={{
-        pathScreen: "home",
+          // ---------- set Caps Inputs
+          const currRoute = 'home';
 
-        styles: [
-          {
-            backgroundColor: "#101",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-          },
+          let args: any = [];
+
+          const screens = [
+            
+
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"home",
+
+          styles:[`{ flex: 1, backgroundColor: "$var_all.colors.primary" }`],
+
+          screenElements:[
+        
+
+    (...args:any) => <Elements.ImageBox pass={{
+      elementsProperties:[{}],
+
+      styles:[{ width: "100%", height: "100%", position: "absolute" }],
+
+      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/flaxboll.appspot.com/o/bg_red.png?alt=media&token=7d654b53-c619-4dc1-94d5-d77fd5cd4420`],
+
+      args,
+    }}/>, 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{ width: "100%", height: 18, backgroundColor: "#000" }`],
+
+            functions:[()=>{}],            childrenItems:[() =><></>],
+
+            args,
+          }}/>
+        , 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  flexDirection: "row",
+  backgroundColor: "$var_all.colors.primary",
+  alignItems: "center",
+  width: "100%",
+}`],
+
+            functions:[()=>{}],            childrenItems:[
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  width: 34,
+  height: 34,
+  alignItems: "center",
+  justifyContent: "center",
+}`],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
+      componentSvg: (Svg:any, SvgObj:any) => {
+        const { Defs, Stop, Path, LinearGradient, G, Circle, Rect, Mask } = SvgObj;
+        return (props:any) => (<Svg
+xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 320 512"
+    fill="red"
+    {...props}
+  >
+    <Path
+      fill="#fff"
+      d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+    />
+  </Svg>)
+      },
+
+      svgOriginal: `
+        <svg></svg>
+      `,
+
+      altura: "14px",
+
+      largura: "14px",
+
+      preenchimento: ['black'],
+
+      args,
+    }}/>],
+
+            args,
+          }}/>
+        , 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  flex: 1,
+  alignItems: "center"
+}`],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{
+  color: "#FFF",
+  textAlign: "left",
+}`
+          ],
+
+          children: [
+            `Now Playing`
+          ],
+
+          args,
+
+        }}/>],
+
+            args,
+          }}/>
+        , 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  width: 34,
+  height: 34,
+  alignItems: "center",
+  justifyContent: "center",
+}`],
+
+            functions:[()=>{}],            childrenItems:[() =><></>],
+
+            args,
+          }}/>
         ],
 
-        screenElements: [
-          (...args: any) => (
-            <Elements.DynView
-              pass={{
-                elementsProperties: ["{}"],
+            args,
+          }}/>
+        , 
+        
 
-                styles: [
-                  `{
-flexDirection: "row",
-backgroundColor: "#0064fe",
-alignItems: "center",
-width: "100%",
-}`,
-                ],
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
 
-                functions: [() => {}],
-                childrenItems: [
-                  (...args: any) => (
-                    <Elements.DynView
-                      pass={{
-                        elementsProperties: ["{}"],
+            styles:[`{ alignItems: "center", justifyContent: "center", minHeight: 220 }`],
 
-                        styles: [
-                          `{
-width: 30,
-height: 30,
-alignItems: "center",
-justifyContent: "center",
-}`,
-                        ],
+            functions:[()=>{}],            childrenItems:[
 
-                        functions: [() => {}],
-                        childrenItems: [
-                          (...args: any) => (
-                            <Elements.SvgView1
-                              pass={{
-                                componentSvg: (Svg: any, SvgObj: any) => {
-                                  const {
-                                    Defs,
-                                    Stop,
-                                    Path,
-                                    LinearGradient,
-                                    G,
-                                    Circle,
-                                    Rect,
-                                    Mask,
-                                  } = SvgObj;
-                                  return (props: any) => (
-                                    <Svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      viewBox="0 0 320 512"
-                                      fill="red"
-                                      {...props}
-                                    >
-                                      <Path
-                                        fill="#fff"
-                                        d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-                                      />
-                                    </Svg>
-                                  );
-                                },
+    (...args:any) => <Elements.ImageBox pass={{
+      elementsProperties:[{}],
 
-                                svgOriginal: `
-<svg></svg>
-`,
+      styles:[{ width: 150, height: 150, borderRadius: 5 }],
 
-                                altura: "16px",
+      URIvariablePath:[`https://4kwallpapers.com/images/wallpapers/lady-gaga-1920x1200-16726.jpeg`],
 
-                                largura: "16px",
+      args,
+    }}/>],
 
-                                preenchimento: ["black"],
+            args,
+          }}/>
+        , 
 
-                                args,
-                              }}
-                            />
-                          ),
-                        ],
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
 
-                        args,
-                      }}
-                    />
-                  ),
-                  (...args: any) => (
-                    <Elements.DynView
-                      pass={{
-                        elementsProperties: ["{}"],
+            styles:[
+        `{
+  flexDirection: "row",
+  alignItems: "center",
+  width: "100%",
+}`, `{ paddingHorizontal: 20 }`],
 
-                        styles: [
-                          `{
-flex: 1,
-}`,
-                        ],
+            functions:[()=>{}],            childrenItems:[
+        
 
-                        functions: [() => {}],
-                        childrenItems: [
-                          (...args: any) => (
-                            <Elements.Text
-                              pass={{
-                                arrProps: ["{}"],
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
 
-                                arrStyles: [
-                                  `{
-color: "#FFF",
-textAlign: "left",
-}`,
-                                ],
+            styles:[`{
+  width: 40,
+  alignItems: "center",
+  justifyContent: "center",
+}`],
 
-                                children: [`Minha Tela`],
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
+      componentSvg: (Svg:any, SvgObj:any) => {
+        const { Defs, Stop, Path, LinearGradient, G, Circle, Rect, Mask } = SvgObj;
+        return (props:any) => (<Svg
+xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <Path
+      stroke="#374151"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.67}
+      d="M17.367 3.842a4.583 4.583 0 0 0-6.484 0L10 4.725l-.883-.883a4.584 4.584 0 1 0-6.484 6.483l.884.883L10 17.692l6.483-6.484.884-.883a4.585 4.585 0 0 0 0-6.483v0Z"
+    />
+  </Svg>)
+      },
 
-                                args,
-                              }}
-                            />
-                          ),
-                        ],
+      svgOriginal: `
+        <svg></svg>
+      `,
 
-                        args,
-                      }}
-                    />
-                  ),
-                ],
+      altura: "14px",
 
-                args,
-              }}
-            />
-          ),
+      largura: "14px",
+
+      preenchimento: [`transparent`],
+
+      args,
+    }}/>],
+
+            args,
+          }}/>
+        , 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{ flex: 1, alignItems: "center" }`],
+
+            functions:[()=>{}],            childrenItems:[
+        (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{ color: "#fff" }`
+          ],
+
+          children: [
+            `Lady Gaga`
+          ],
+
+          args,
+
+        }}/>, (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{ color: "#263238", fontSize: 12 }`
+          ],
+
+          children: [
+            `Just Dance`
+          ],
+
+          args,
+
+        }}/>],
+
+            args,
+          }}/>
+        , 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  width: 40,
+  alignItems: "center",
+  justifyContent: "center",
+}`],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
+      componentSvg: (Svg:any, SvgObj:any) => {
+        const { Defs, Stop, Path, LinearGradient, G, Circle, Rect, Mask } = SvgObj;
+        return (props:any) => (<Svg
+xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <Path
+      stroke="#374151"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M18 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM18 22a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM8.59 13.51l6.83 3.98M15.41 6.51l-6.82 3.98"
+    />
+  </Svg>)
+      },
+
+      svgOriginal: `
+        <svg></svg>
+      `,
+
+      altura: "14px",
+
+      largura: "14px",
+
+      preenchimento: [`transparent`],
+
+      args,
+    }}/>],
+
+            args,
+          }}/>
         ],
 
-        functions: [() => {}],
+            args,
+          }}/>
+        ],
 
-        args,
-      }}
-    />
-  ),
-];
+          functions:[()=>{}],
 
-const initCt = () => ({
-  all: {
-    colors: {
-      primary: "#fff",
-      secondary: "red",
-    },
-    cond1: false,
-    func1: (nativeEvent) => console.log(nativeEvent),
-    Comp1: <RN.Text style={{ color: "darkred" }}>---</RN.Text>,
-    lists: {
-      lt1: [
-        {
-          name: "João",
-          img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        { name: "Luciana" },
-        { name: "Pedro" },
-      ],
-      lst2: [
-        { name: "Tarefa 1", date: "21/03/25" },
-        { name: "Tarefa 2", date: "21/03/25" },
-        { name: "Tarefa 3", date: "24/03/25" },
-      ],
-      lst3: [{ name: "Task 1" }, { name: "Task 2" }],
-    },
-    name: "Name:",
-    dataToSet: { name: "Carlos", phone: "1111" },
-  },
-});
-const initObj = initCt();
-// console.log(initObj);
+          args,
+        }}/>
+          ];
 
-const arrInitFuncs = [() => {}];
+          const initCt = () => (
+ {
+ 'all': { 
+'colors': { 
+'primary': "#000", 'secondary': "red" } 
+, 
+'cond1': false, 
+'func1': (nativeEvent)=>console.log(nativeEvent), 
+'Comp1': <RN.Text style={{color: "darkred"}}>---</RN.Text>, 
+'lists': { 
+'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}], 
+'lst2': [{name: "Tarefa 1", date: "21/03/25"},{name: "Tarefa 2", date: "21/03/25"},{name: "Tarefa 3", date: "24/03/25"}], 'lst3': [{name: "Task 1"},{name: "Task 2"}] } 
+, 
+'name': "Name:", 'dataToSet': {name: "Carlos", phone: "1111"} } 
+ 
+} 
+ );
+          const initObj = initCt();
+          // console.log(initObj);
 
-export const useRoutes = create(() => ({ currRoute }));
-export const useData = create(() => initObj);
+          const arrInitFuncs = [
+            ()=>{}
+          ];
 
-// ---------- set Main Component
-export const Router = () => {
-  return (
-    <Project
-      configData={{
-        screens,
-        initCt,
-        arrInitFuncs,
-      }}
-    />
-  );
-};
+          export const useRoutes = create(() => ({ currRoute }));
+          export const useData = create(() => initObj);
+
+          // ---------- set Main Component
+          export const Router = () => {
+            return (
+              <Project
+                configData={{
+                  screens,
+                  initCt,
+                  arrInitFuncs,
+                }}
+              />
+            );
+          };
+        
